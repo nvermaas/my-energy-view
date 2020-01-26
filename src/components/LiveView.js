84698fc03ac6d105ab72f1df89f9ea28b4c9dfa2
Nico Vermaas <vermaas@astron.nl>
2020-01-26 11:57:18 +0100

    - adding solar panel data

--- src/components/LiveView.js
+++ src/components/LiveView.js
@@ -39,8 +39,9 @@ export default function LifeView(props) {
     let renderLiveInfo
     if (data!==undefined) {
         let PowerData = parseInt(data['data'].power_usage) - parseInt(data['data'].power_delivery)
-        let CurrentSolar = (data['data'].current_power_growatt_label)
-        let CurrentUsage = (data['data'].real_power_usage)
+        let CurrentSolar = Math.round(data['data'].current_power_growatt_label)
+        let CurrentUsage = Math.round(data['data'].real_power_usage)
+
         renderLiveInfo =
             <div>
                 <Card border="warning">
